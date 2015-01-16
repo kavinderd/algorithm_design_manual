f3650af2e7cefd1a071e21f61e6e0c978aff7ade
Kavinder Dhaliwal <kavinderd@gmail.com>
2015-01-16 14:19:04 -0800

    come back to ex 4-27

---
+++ chapter_4/exercises/ex4-27.rb
@@ -0,0 +1,9 @@
+# Exercise 4-27
+### Problem ###
+# Let P be a simple but not necessarily convex, polygon and q an arbitrary point not necessaryily in P. Design an 
+# efficient algorithm to find a line segment originating from q that intersects the maximum number of edges of P. Inother
+# words, if standing at point q, in what diretion should you aim a gun so the bullet will through the largest number
+# of walls. A bullet through a vertex of P gets credit for only one wall. An O(nlogn) algorithm is possible.
+### End ###
+#
+# Huh?
