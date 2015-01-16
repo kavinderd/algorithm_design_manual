53d29009ab3bc90df819ea2c5dbd95f359db52f7
Kavinder Dhaliwal <kavinderd@gmail.com>
2015-01-16 14:28:53 -0800

    come back to ex 4-28

---
+++ chapter_4/exercises/ex4-28.rb
@@ -0,0 +1,8 @@
+# Exercise 4-28
+### Problem ###
+# In one of my research papers, I discovered a comparison-based sorting algorithm
+# that runs in O(nlog(sqrt(n))). Given the existence of an Omega(nlogn) lower bound
+# for sorting, how can this be possible.
+### End ###
+#
+# Huh ?
