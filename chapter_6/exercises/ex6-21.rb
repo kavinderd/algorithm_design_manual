67b94ad3fcd22251c33847243f98987790da49c3
Kavinder Dhaliwal <kavinderd@gmail.com>
2015-03-26 14:08:34 -0700

    partial solution to exercise 6-21

---
+++ chapter_6/exercises/ex6-21.rb
@@ -0,0 +1,11 @@
+# Exercise 6-21
+### Problem ###
+# Let G=(V,E) be a weighted acyclic directed graph with possible negative edge weights.
+# Design a linear-time algorithm to solve the single-source shortest-path problem
+# from a given source v.
+### End ###
+#
+# First Topological sort the graph.
+# Then run Dijkstra's algorithm in topological order
+#
+# Will implement later
