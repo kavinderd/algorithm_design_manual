b079398b11bca2e1cda6160c62f74c6e035607c7
Kavinder Dhaliwal <kavinderd@gmail.com>
2015-03-26 12:36:34 -0700

    partial solution to exercise 6-14

---
+++ chapter_6/exercises/ex6-14.rb
@@ -0,0 +1,9 @@
+# Exercise 6-14
+### Problem ###
+# The single-destination shortest path problem for a directed graph seeks the shortest path from every vertex
+# to a specified vertex v. Give an efficient algorithm to solve the single-destination shortest paths problem
+### End ###
+#
+# - Run Dijkstra's algorithm for each vertex from that vertex to the destination vertex.
+# Will implement later
+#
