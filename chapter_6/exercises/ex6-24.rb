0d54b66ac74fe6ea21411d2d294c2047a8f811dc
Kavinder Dhaliwal <kavinderd@gmail.com>
2015-03-26 14:16:44 -0700

    partial solution to exercise 6-24

---
+++ chapter_6/exercises/ex6-24.rb
@@ -0,0 +1,12 @@
+# Exercise 6-24
+### Problem ###
+# A matching in a graph is a set of disjoint edges. i.e. edges that do not share
+# any vertices in common. Give a linear-time algorithm to find a maximum matching 
+# in a tree
+### End ###
+# 
+# iterate over every vertex
+# if that vertice is in the set skip it
+# else add one of its edges to another vertex that's also not in the set
+#
+# Will implement later
